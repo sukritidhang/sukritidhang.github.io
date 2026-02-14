df3c4ac88f9ef25b3d9136bf73140a3267eadaf0
sukritidhang <sukritidhang@gmail.com>
2026-02-14 11:56:04 +0530

    Files added

--- js/index.js
+++ js/index.js
@@ -2,13 +2,25 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuBtn = document.querySelector(".menu-toggle");
   const navLinks = document.querySelector(".nav-links");
+  const links = document.querySelectorAll(".nav-links a");
 
   if (menuBtn) {
     menuBtn.addEventListener("click", function () {
       navLinks.classList.toggle("show");
     });
   }
+
+
+
+/* Close Menu When Link Clicked (Mobile) */
+  links.forEach(link => {
+    link.addEventListener("click", function () {
+      navLinks.classList.remove("show");
+    });
+  });
+
 });
+
 
 
 /* Modal */
