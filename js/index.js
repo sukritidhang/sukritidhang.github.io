534c1967b99e78b2162e1ca25185e3b2e5e1a903
sukritidhang <sukritidhang@gmail.com>
2026-03-23 11:30:33 +0530

    Files modified

--- js/index.js
+++ js/index.js
@@ -104,3 +104,20 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
+function openYear(evt, yearName) {
+  let tabcontent = document.getElementsByClassName("tabcontent");
+  let tablinks = document.getElementsByClassName("tablinks");
+
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  document.getElementById(yearName).style.display = "block";
+  evt.currentTarget.classList.add("active");
+}
